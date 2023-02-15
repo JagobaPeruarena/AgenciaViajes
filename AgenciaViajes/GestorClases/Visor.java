ab92c4056a6ae39184c4e6215487798af5da9ea9
Jagoba Peruarena Martinez <Jagoba Peruarena Martinez@HZ114822.H012108.NET>
2023-02-15 14:26:52 +0100

    ocho punto uno commit

--- AgenciaViajes/GestorClases/Visor.java
+++ AgenciaViajes/GestorClases/Visor.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 import Clases.clientes;
 import Clases.hoteles;
+import Clases.reservas;
 
 public class Visor {
 	public static void mostrarMensaje(String mensaje) {
@@ -21,6 +22,11 @@ public class Visor {
 	public static void mostrarHoteles(ArrayList<hoteles> hoteles) {
 		for (hoteles hotel: hoteles) {
 			System.out.println(hotel.toString());
+		}
+	}
+	public static void mostrarReserva(ArrayList<reservas> reservas) {
+		for (reservas reserva: reservas) {
+			System.out.println(reserva.toString());
 		}
 	}
 }
